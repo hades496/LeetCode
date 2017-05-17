9cbcaa1982b79228c102f715623bc7be4dafbb55
hades <328705509@qq.com>
2017-05-17 13:33:37 +0800

    Update 11. Container With Most Water.py
    a better style

--- 11. Container With Most Water.py
+++ 11. Container With Most Water.py
@@ -4,22 +4,11 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        l=0
-        r=len(height)-1
-        ans = min(height[l],height[r])*(r-l)
-        while (True):
-            print 'l=%d r=%d'%(l,r)
-            if (height[l]<height[r]):
-                i=l
-                while (height[i]<=height[l] and i<r) : i+=1
-                if l<i and i<r :l=i
-                else : break
-            else:
-                i=r
-                while (height[i]<=height[r] and l<i) : i-=1
-                if l<i and i<r :r=i
-                else: break
-            temp = min(height[l],height[r])*(r-l)
-            if (temp>ans):
-                ans=temp
+        l = 0
+        r = len(height) - 1
+        ans = 0
+        while (l<r):
+            ans = max(ans, min(height[l],height[r])*(r-l))
+            if (height[l]<height[r]): l+=1
+            else: r-=1
         return (ans)
